c33117d10ba3cecdea64b3d181607249c25d9a55
halid aslan <77991418+halidaslan@users.noreply.github.com>
2022-09-26 17:10:29 +0300

    Update Bolum8_Fonksiyonlar.cpp

--- Bolum8_Fonksiyonlar.cpp
+++ Bolum8_Fonksiyonlar.cpp
@@ -1,8 +1,55 @@
-﻿#include<stdio.h>
+#include<stdio.h>
 #include<iostream>
 
 
 using namespace std;
+
+// Asal sayıları veren kod
+
+bool asalmi(int x) {
+	for (int i = 2; i < x; i++) {
+		if (x % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	
+	//cout << asalmi(19) << endl;
+
+	int count = 1;
+	for (int i = 2; count <= 20; i++) {
+		if(asalmi(i)){
+			cout << count << ". asal sayi : " << i << endl;
+			count++;
+		}
+	}
+	return 0;
+}
+
+/*
+int ust(int t, int u) {
+	int sonuc = 1;
+
+	for (int i = 1; i <= u; i++) {
+		sonuc = sonuc * i;
+	}
+	return sonuc;
+}
+
+int main() {
+	//İlk 20 Mersanne sayısını veren kod (2^n)-1...
+
+	for (int i = 1; i <= 20; i++) {
+		cout << ust(2, i)-1 << endl;
+	}
+	return 0;
+}*/
+
+
+/*
 int faktoriyelr(int n) {
 	if (n == 1)
 		return 1;
@@ -35,7 +82,7 @@ int main() {
 	cout << kombinasyon(5, 2) << endl;
 	cout << faktoriyelr(5) << endl;
 	cout << func(4) << endl;
-
+	
 	return 0;
 
-}
+}*/
